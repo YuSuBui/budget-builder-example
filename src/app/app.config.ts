80b9b2609183577255fd4b9a706543f207b559e6
yusubui <buicongsum2001@gmail.com>
2025-04-21 14:33:13 +0700

    feat: add new primeNG package

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideAnimations(), providePrimeNG({
+    theme: {
+      preset: Aura, options: {
+        darkModeSelector: '.my-app-light'
+      }
+    }
+  })]
 };
